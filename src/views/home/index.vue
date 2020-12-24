d533b7565974465c0fd57e44453b1f3fc2023036
CallMe-Jason <2872213719@qq.com>
2020-12-24 10:53:16 +0800

    init

---
+++ src/views/home/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home-container">首页组件</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIndex',
+  props: {
+
+  },
+  components: {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="login-container">登录组件</div>
+</template>
+
+<script>
+export default {
+  name: 'LoginIndex',
+  props: {
+
+  },
+  components: {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
